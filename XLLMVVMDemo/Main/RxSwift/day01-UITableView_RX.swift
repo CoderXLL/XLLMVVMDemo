af2b03000f51459e510e970f720ef5a72baeba0b
肖乐 <xiaole@xiaoyueledeMac-Pro.local>
2021-06-04 16:37:07 +0800

    临时提交

--- XLLMVVMDemo/Main/RxSwift/day01-UITableView_RX.swift
+++ XLLMVVMDemo/Main/RxSwift/day01-UITableView_RX.swift
@@ -29,6 +29,20 @@ extension UILabel {
     }
 }
 
+extension Reactive where Base: UILabel {
+    public var fontSize: Binder<CGFloat> {
+        return Binder(self.base) { label, fontSize in
+            label.font = .systemFont(ofSize: fontSize)
+        }
+    }
+    
+    public var text: Binder<String?> {
+        return Binder(self.base) { label, text in
+            label.text = text
+        }
+    }
+}
+
 class XLLViewController01: UIViewController {
     let disposeBag = DisposeBag()
     
@@ -38,6 +52,8 @@ class XLLViewController01: UIViewController {
         view.addSubview(self.tableView)
         view.addSubview(self.test1Label)
         
+        
+        //observable的binder/subscribe
         viewModel.data.bind(to: tableView.rx.items(cellIdentifier: "hahaha")) { _, item, cell in
             cell.textLabel?.text = item
         }.disposed(by: disposeBag)
@@ -59,11 +75,11 @@ class XLLViewController01: UIViewController {
         }.disposed(by: disposeBag)
         
         
-        Observable.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).filter({
-            return $0 < 10
-        }).bind {
-            self.test1Label.text = "\($0)"
-        }.disposed(by: disposeBag)
+//        Observable.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).filter({
+//            return $0 < 10
+//        }).bind {
+//            self.test1Label.text = "\($0)"
+//        }.disposed(by: disposeBag)
         
         
         Observable.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).map({
@@ -72,10 +88,35 @@ class XLLViewController01: UIViewController {
             self.test1Label.textColor = $0 ? .green : .blue
         }.disposed(by: disposeBag)
         
+        // 通过UI扩展，去处理对应的Binder
+//        Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).map({
+//            return CGFloat($0)
+//        }).bind(to: self.test1Label.fontSize).disposed(by: disposeBag)
+        // 通过Reactive扩展，去处理对应的Binder
         Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).map({
             return CGFloat($0)
-        }).bind(to: self.test1Label.fontSize).disposed(by: disposeBag)
+        }).bind(to: self.test1Label.rx.fontSize).disposed(by: disposeBag)
         
+        Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance).map({
+            return "\($0)"
+        }).bind(to: self.test1Label.rx.text).disposed(by: disposeBag)
+        
+        
+        //既是订阅者，又是观察者的Subjects
+        //一共有4中Subjects  PublishSubject、BehaviorSubject、ReplaySubject、Variable
+        //创建一个PublishSubject
+        let subject = PublishSubject<String>()
+         
+        //subject完成后它的所有订阅（包括结束后的订阅），都能收到subject的.completed事件，
+        subject.subscribe(onNext: { string in
+            print("第1次订阅：", string)
+        }, onCompleted:{
+            print("第1次订阅：onCompleted")
+        }).disposed(by: disposeBag)
+        //当前有1个订阅，则该信息会输出到控制台
+        subject.onNext("333")
+        //让subject结束
+        subject.onCompleted()
         
     }
     
